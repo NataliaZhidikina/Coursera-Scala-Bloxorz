5e4a9bb8f7d0a47f29e8c99316fea9a483d1c76d
truevines <truevines@gmail.com>
2016-11-15 19:47:46 -0800

    my solution

--- src/main/scala/streams/Solver.scala
+++ src/main/scala/streams/Solver.scala
@@ -92,37 +92,34 @@ trait Solver extends GameDef {
   def from(initial: Stream[(Block, List[Move])],
            explored: Set[Block]): Stream[(Block, List[Move])] = {
 
-    val current = initial.head
+    //current set = initial
+    //find next set
+    // recurvive:  current#::from(next)
 
-    if (done(current._1)) {
-      return initial
-    } else {
-      //breadth first => must check whether any of the neighbors is goal, exhaust all, the move on to next level
-      //depth firs => check if a neighbor is goal, if not, then check its neighbors; repeat for all neighbors
-      
-      val currentNeighbors = neighborsWithHistory(current._1, current._2)
-      val currentNewNeighbors = newNeighborsOnly(currentNeighbors, explored)
+    //
+    def getNewNeighbors (x: (Block, List[Move])): Stream[(Block, List[Move])] = {
+      newNeighborsOnly(neighborsWithHistory(x._1, x._2), explored)
+    }
+    //generating the next set 
 
-      val goalContainedInNeighbors = currentNewNeighbors.filter((x: (Block, List[Move])) => done(x._1))
+    if (initial.isEmpty) {return Stream.empty} else {
 
-      if(goalContainedInNeighbors != Nil) {
-        return goalContainedInNeighbors(0)::initial
-      } else {
-        //traverse each neighbor 
-          from(n#::initial, explored.current._1)
+          lazy val next = for { 
+                            blk <- initial
+                            nbr <- getNewNeighbors(blk)
+                         } yield nbr
+          
+          //not functional, lmited number
+          val initialBlock = initial.unzip._1
+          var newExplored = explored
+          for (x <- initialBlock) {
+            newExplored += x
+          }
 
-      }
-
-      //for each neighbor, neighbor becomes current, initial stream = neighbor + initial stream
-      // explored = explored + current
-      //repeat from
-
+          return initial #::: (from(next, newExplored))
     }
 
-    return ???
-
   }
-
 
 
 
@@ -130,13 +127,32 @@ trait Solver extends GameDef {
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = {
+
+
+    val initial = Stream((startBlock, List[Move]()))
+    val explored = Set[Block]()
+
+    lazy val paths = for {
+                          x <- from(initial, explored)
+                          } yield x
+    paths
+
+  }
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+
+    lazy val goal = for {
+      x <- pathsFromStart
+      if (done(x._1))
+    } yield x
+
+    goal
+  }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -146,5 +162,14 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = {
+
+    val paths = pathsToGoal
+
+    if(paths.isEmpty) {List.empty} else {
+
+      paths(0)._2.reverse
+    }
+
+  }
 }
